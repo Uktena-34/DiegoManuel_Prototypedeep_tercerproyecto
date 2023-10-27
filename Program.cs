1abde15a9a7d0595bda30d9dabac28f57b6b6b04
Uktena-34 <dgomezamaton9@gmail.com>
2023-10-27 17:51:47 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoManuel_Prototypedeep_tercerproyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal oAnimal = new Animal() { Nombre = "Oveja Dolly", Patas = 4 };
+            oAnimal.Rasgos = new Detalles();
+            oAnimal.Rasgos.Color = "Blanca";
+            oAnimal.Rasgos.Raza = "Oveja";
+
+            Animal oAnimalClonado = oAnimal.Clone() as Animal;
+            oAnimalClonado.Rasgos.Color = "Negro";
+            oAnimalClonado.Nombre = "Oveja negra";
+
+            Console.WriteLine("animal original: " + oAnimal.Rasgos.Color);
+            Console.WriteLine("annimal clonado: " + oAnimalClonado.Rasgos.Color);
+            Console.WriteLine("animal original: " + oAnimal.Nombre);
+            Console.WriteLine("animal clonado: " + oAnimalClonado.Nombre);
+        }
+    }
+}
---
+++ Prototype_deep.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoManuel_Prototypedeep_tercerproyecto
+{
+    public class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+        public string Nombre { get; set; }
+
+        public Detalles Rasgos { get; set; }
+
+
+        public object Clone()
+        {
+            Animal clonado = this.MemberwiseClone() as Animal;
+            Detalles detalles = new Detalles();
+            detalles.Color = this.Rasgos.Color;
+            detalles.Raza = this.Rasgos.Raza;
+            clonado.Rasgos = detalles;
+            return clonado;
+        }
+    }
+
+    public class Detalles
+    {
+        public string Color { get; set; }
+        public string Raza { get; set; }
+    }
+
+}
